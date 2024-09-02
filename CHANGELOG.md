93e8cb92fe4ae3ba6512fdf4ee0f5bc7c684b1fb
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-02 12:02:07 -0700

    Update `@shgysk8zer0/jwk-utils` for easier key management

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [v1.1.8] - 2024-09-02
+
+### Added
+- Add test for exporting public JWK as file
+
+### Changed
+- Update `@shgysk8zer0/jwk-utils` for easier key management
+
 ## [v1.1.7] - 2024-09-01
 
 ### Changed
--- api/jwtgen.js
+++ api/jwtgen.js
@@ -1,18 +1,14 @@
 import { createHandler } from '../handler.js';
 import { PRIVATE_KEY } from '../keys.js';
 import { importJWK } from '@shgysk8zer0/jwk-utils/jwk.js';
-import { decodeToken } from '@shgysk8zer0/jwk-utils/jwt.js';
 import { createOriginAuthToken } from '@shgysk8zer0/jwk-utils/origin-tokens.js';
 
 export default createHandler({
 	async get(req) {
-		const privateKey = await importJWK(PRIVATE_KEY);
+		const [privateKey] = await Promise.all([importJWK(PRIVATE_KEY)]);
 
 		const token = await createOriginAuthToken(URL.parse(req.url)?.origin, privateKey);
 
-		return Response.json({
-			token,
-			decoded: decodeToken(token),
-		});
+		return Response.json({ token, });
 	}
 });
