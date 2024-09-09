683d4cd3b729efe1706517ecbafe755687e4df43
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-08 19:12:15 -0700

    More misc updates
    Significant updates in `@shgysk8zer0/jwk-utils`

--- api/jwk.js
+++ api/jwk.js
@@ -1,11 +1,11 @@
 import { createHandler } from '../handler.js';
 import { PUBLIC_KEY } from '../keys.js';
-import { importJWK, createJWKFile } from '@shgysk8zer0/jwk-utils/jwk.js';
+import { importJWK, createJWKBlob } from '@shgysk8zer0/jwk-utils/jwk.js';
 
 export default createHandler({
 	async get() {
 		const key = await importJWK(PUBLIC_KEY);
-		const file = await createJWKFile(key);
+		const file = await createJWKBlob(key);
 		return new Response(file);
 	}
 });
